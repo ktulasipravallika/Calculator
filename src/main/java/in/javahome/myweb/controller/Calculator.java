0e3b827cef28aec1f8f8272d70c5151f54032589
ktulasipravallika <122127780+ktulasipravallika@users.noreply.github.com>
2024-02-08 18:18:23 -0800

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -1,10 +1,12 @@
 package in.javahome.myweb.controller;
 /*
- * 
+ * Calculator Class to calculate and return
+ * the correct values for
+ * addition, subtraction, multiplication, division.
  */
 public class Calculator {
 	
-	/*
+	/* Method to perform Addition
 	 * @param i
 	 * @param j
 	 * @return int
@@ -12,11 +14,34 @@ public class Calculator {
 	public int add(int i, int j){
 		return i+j;
 	}
+
+	/* Method to perform Multiplication
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+
 	public int multiply(int i, int j){
 		return i*j;
 	}
-	
+
+	/* Method to perform Subtraction
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+
 	public int sub(int i, int j){
 		return i-j;
+	}
+
+	/* Method to perform Division
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+
+	public int division(int i, int j){
+		return i/j;
 	}
 }
